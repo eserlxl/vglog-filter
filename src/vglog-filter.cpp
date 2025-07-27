842defd69a12ef1e569b73eda65e44f851536825
Eser KUBALI <eserlxl@gmail.com>
2025-07-27 18:06:30 +0300

    Update license headers: replace (C) with © symbol

--- src/vglog-filter.cpp
+++ src/vglog-filter.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Eser KUBALI <lxldev.contact@gmail.com>
+// Copyright © 2025 Eser KUBALI <lxldev.contact@gmail.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 //
 // This file is part of vglog-filter and is licensed under
