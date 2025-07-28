ae27a6646b36ad0fd206e5a691f516eaa0606a0d
Eser KUBALI <eserlxl@gmail.com>
2025-07-29 00:17:42 +0300

    Add test header with prototype

--- src/test_header.h
+++ src/test_header.h
@@ -1,6 +1,7 @@
 #ifndef TEST_HEADER_H
 #define TEST_HEADER_H
 
-// Test function prototype removed
+// Test function prototype
+int test_function(int param1, const char* param2);
 
 #endif // TEST_HEADER_H
