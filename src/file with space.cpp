f3507c151842974f72d5b67e6a946d3fa13baae5
Eser KUBALI <eserlxl@gmail.com>
2025-07-29 02:06:48 +0300

    Add CLI options to source file

--- src/file with space.cpp
+++ src/file with space.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <getopt.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    int opt;
+    while ((opt = getopt(argc, argv, "hv")) != -1) {
+        switch (opt) {
+            case 'h':
+                std::cout << "Help" << std::endl;
+                break;
+            case 'v':
+                std::cout << "Version" << std::endl;
+                break;
+        }
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
