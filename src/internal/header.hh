a920fa61c4d18e222daea97d5c59eb3f41d56f50
Eser KUBALI <eserlxl@gmail.com>
2025-07-28 23:27:30 +0300

    Add internal header

---
+++ src/internal/header.hh
@@ -0,0 +1,13 @@
+#ifndef INTERNAL_HEADER_HH
+#define INTERNAL_HEADER_HH
+
+// Function prototype that could be removed to test API breaking detection
+int process_data(const char* input, int length);
+
+// Another function that could be removed
+void cleanup_resources(void);
+
+// This function will be removed to test API breaking detection
+int deprecated_function(int param);
+
+#endif // INTERNAL_HEADER_HH 
