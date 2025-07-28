87eee2ef52baa477e21ecd3ffcfc8ba2467a79a5
Eser KUBALI <eserlxl@gmail.com>
2025-07-29 02:24:30 +0300

    Remove function prototype

--- src/test_header.h
+++ src/test_header.h
@@ -1,7 +1,6 @@
 #ifndef TEST_HEADER_H
 #define TEST_HEADER_H
 
-// Test function prototype
-int test_function(int param1, const char* param2);
+// Test function prototype removed
 
 #endif // TEST_HEADER_H
