420fdbda8af50a8917d08a21237226cfd1cf8ac5
Eser KUBALI <eserlxl@gmail.com>
2025-07-29 08:32:41 +0300

    Add source file with space in name

--- src/file with space.cpp
+++ src/file with space.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
-#include <getopt.h>
 
-int main(int argc, char* argv[]) {
-    int opt;
-    while ((opt = getopt(argc, argv, "hv")) != -1) {
-        switch (opt) {
-            case 'h':
-                std::cout << "Help" << std::endl;
-                break;
-            case 'v':
-                std::cout << "Version" << std::endl;
-                break;
-        }
-    }
+int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
